96679563cc5a71c987982bc97ec05c1363c609a1
Kelvin-Ofili <kelvinofili1@gmail.com>
2022-06-03 12:33:23 +0000

    found

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,26 +1,21 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ *
+ * Return: Always 0 (Sucess)
  */
+
 int main(void)
 {
-<<<<<<< HEAD
-int num;
-num = 97;
-while (num <= 122)
-putchar(num);
-num++;
-=======
-int p;
-p = 97;
-while (p <= 122)
-{
-putchar(p);
-p++;
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
 }
->>>>>>> c38d008b18676f16774155151031a0fcc1c46752
-putchar('\n');
-return (0);
-}
+
