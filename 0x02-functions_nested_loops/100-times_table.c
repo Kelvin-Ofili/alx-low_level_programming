5d822f489704783c3d9fcd8d9a0585005534b0f7
Kelvin-Ofili <kelvinofili1@gmail.com>
2022-06-07 19:33:40 +0000

    switch

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,11 +22,11 @@ void print_times_table(int n)
 		op = a * b;
 		_putchar(44);
 		_putchar(32);
-		if (op <= 10)
+		if (op <= 9)
 		{
 		_putchar(32);
 		_putchar(32);
-		_putchar(op + 48);
+		_putchar(op + 48)
 		}
 		else if (op <= 99)
 		{
@@ -38,7 +38,7 @@ void print_times_table(int n)
 		{
 		_putchar(((op / 100) % 10) + 48);
 		_putchar(((op / 10) % 10) + 48);
-		_putchar((op % 10) + 48);
+		_putchar(((op % 10) + 48);
 		}
 	}
 	_putchar('\n');
